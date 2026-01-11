58bbb1156e2bf2b4c27b012bd66f108badf3b9a5
sivaramakrishnatadikonda <sivaramakrishnatadikonda123@gmail.com>
2026-01-11 16:00:39 +0530

    installed nginx

--- 03.siva.sh
+++ 03.siva.sh
@@ -11,7 +11,7 @@ else
 fi
 
 VALIDATE(){
-    
+
      if [ $1 - ne 0 ]
     then
         echo "$2 installed -------- sucessfully"
@@ -30,11 +30,12 @@ then
     echo "nginx is not installed please install"
 
     dnf install nginx -y
+
     VALIDATE $? "nginx"
 
 else
     echo "nginx is installed nothing to do"
+    exit 1
 fi
-
 
 
