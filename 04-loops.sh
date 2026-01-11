7fe6a07ee1c1e807bd3999f20890db7ac0d249a8
sivaramakrishnatadikonda <sivaramakrishnatadikonda123@gmail.com>
2026-01-11 16:34:37 +0530

    install nginx and mysql

--- 04-loops.sh
+++ 04-loops.sh
@@ -13,3 +13,11 @@ then
 else
     echo -e  " $G you are already run the script with root access $N "
 fi
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then
+    echo -e  "$R if mysql is not installed ------ please install $N"
+else
+    echo  -e " $Y if mysql is installed ------- nothing to do $N"
+fi
