bd18317a0a4ca189b4ea075f7abec93eee26d998
sivaramakrishnatadikonda <sivaramakrishnatadikonda123@gmail.com>
2026-01-11 16:48:40 +0530

    install nginx and mysql

--- 04-loops.sh
+++ 04-loops.sh
@@ -19,15 +19,17 @@ dnf list installed mysql
 if [ $? -ne 0 ]
 then
     echo -e  "$R if mysql is not installed ------ please install $N"
+    
+    dnf install mysql -y
+    
+    if [ $? -eq 0 ]
+    then
+    echo -e " $G mysql is installed ------ install sucessfully $N"
+    else
+    echo -e  " $R mysql is not installed ----- failure $N"
+    exit 1
+    fi
 else
     echo  -e " $Y if mysql is installed ------- nothing to do $N"
 fi
 
-dnf install mysql -y
-if [ $? -eq 0 ]
-then
-    echo -e " $G mysql is installed ------ install sucessfully $N"
-else
-    echo -e  " $R mysql is not installed ----- failure $N"
-    exit 1
-fi
