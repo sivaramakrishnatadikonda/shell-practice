a2a3a03bf988abdeec60571f7f2a2da6fcbbcc37
sivaramakrishnatadikonda <sivaramakrishnatadikonda123@gmail.com>
2026-01-11 16:22:26 +0530

    install nginx and mysql

--- 02-functios.sh
+++ 02-functios.sh
@@ -6,9 +6,10 @@ if [ $USERID -ne 0 ]
 
 then
     echo "ERROR:: please run the script with root access"
+     exit 1
 else  
     echo "Script is running with root access"
-    exit 1
+   
 fi
 
 VALIDATION(){
