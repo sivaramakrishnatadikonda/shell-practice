fa9500ec585d60a50edc694695567df9e18bbe1e
sivaramakrishnatadikonda <sivaramakrishnatadikonda123@gmail.com>
2026-01-11 16:30:12 +0530

    install nginx and mysql

--- 04-loops.sh
+++ 04-loops.sh
@@ -8,8 +8,8 @@ N="\e[0m"
 
 if [ $USERID -ne 0 ]
 then 
-    echo "$R ERROR:: please run script with root access $N"
+    echo " $R ERROR:: please run script with root access $N "
     exit 1
 else
-    echo "$G you are already run the script with root access $N"
+    echo " $G you are already run the script with root access $N "
 fi
