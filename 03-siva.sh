877b8cc3d1d11360cda385715ac7b0160ac8813a
sivaramakrishnatadikonda <sivaramakrishnatadikonda123@gmail.com>
2026-01-12 17:37:47 +0530

    install nginx and mysql

--- 03-siva.sh
+++ 03-siva.sh
@@ -5,8 +5,8 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-LOGS_FOLDER= "/var/log/shellscripts-logs"
-SCRIPT_NAME= $(echo $0 | cut -d "." -f1)
+LOGS_FOLDER="/var/log/shellscripts-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 PACKAGES=("mysql" "nginx" "mongodb")
 
